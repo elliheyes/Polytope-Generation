4272e55f53241fdf2beb9d206a41028964b17484
Elli Heyes <44784866+elliheyes@users.noreply.github.com>
2023-03-21 14:12:10 +0000

    Update gen_poly.c

--- C/gen_poly.c
+++ C/gen_poly.c
@@ -11,14 +11,14 @@ int main (int narg, char* fn[]){
   int i, n;
   
   /* repeated evolution of a random initial population, extracting terminal states */
-  FILE * fp1 = fopen("Num_Terminal_States.txt","w");
+  FILE * fp1 = fopen("4d_5v_Num_Terminal_States.txt","w");
   struct bitlist * bl = searchenv(100, 50, NUMGEN, POPSIZE, ROULETTE, NUMCUTS, KEEPFITEST, MUTRATE, ALPHA, MONITORON, fp1, &n); 
   fclose(fp1); 
 
   /* write terminal states to a file */
-  FILE * fp2 = fopen("Terminal_States.txt","w");  
+  FILE * fp2 = fopen("4d_5v_Terminal_States.txt","w");  
   for(i=0; i<n; i++) fprintbitlist(fp2, bl[i]); 
   fclose(fp2); 
-  
+
   return 0;
 } 
