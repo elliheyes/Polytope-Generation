1f908bc7c517aa5b9362912e6c338ea745bf3608
Elli Heyes <44784866+elliheyes@users.noreply.github.com>
2023-04-18 18:33:09 +0100

    Update gen_poly.c

--- C/gen_poly.c
+++ C/gen_poly.c
@@ -8,17 +8,10 @@
 
 int main (int narg, char* fn[]){
 
-  int i, n;
+  int n;
   
   /* repeated evolution of a random initial population, extracting terminal states */
-  FILE * fp1 = fopen("4d_5v_Num_Terminal_States.txt","w");
-  struct pointlist * pl = searchenv(10, 1, NUMGEN, POPSIZE, ROULETTE, NUMCUTS, KEEPFITEST, MUTRATE, ALPHA, MONITORON, fp1, &n); 
-  fclose(fp1); 
-
-  /* write terminal states to a file */
-  FILE * fp2 = fopen("4d_5v_Terminal_States.txt","w");  
-  for(i=0; i<n; i++) fprintpointlist(fp2, pl[i]); 
-  fclose(fp2); 
+  struct pointlist * pl = searchenv(1000, 1, NUMGEN, POPSIZE, ROULETTE, NUMCUTS, KEEPFITEST, MUTRATE, ALPHA, MONITORON, &n); 
 
   return 0;
 } 
