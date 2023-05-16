f1a28bcef42935aeafeb605216e8ddf06252a16e
Elli Heyes <44784866+elliheyes@users.noreply.github.com>
2023-05-16 14:06:20 +0100

    Update Global.h

--- C/Global.h
+++ C/Global.h
@@ -1,5 +1,5 @@
 /*  ======================================================================  */
-/*  ==========	     			   	  	        ==========  */
+/*  ==========	     			   	  	       	==========  */
 /*  ==========                    G L O B A L                   ==========  */
 /*  ==========						        ==========  */
 /*  ======================================================================  */
@@ -16,16 +16,11 @@
 
 /*  ============           	P A R A M E T E R S              ============  */
 
-
-#define	Long long long    
-#define LLong long long
-
-
-#define MIN -15	          /* lower bound on integers considered for points */
-#define MAXNVRTS 5  	  /* maximum number of vertices */
-#define POLYDIM 4         /* dimension of polytopes */
-#define BINLEN 5          /* maximum length of binary number */
-#define POPSIZE 200       /* population size */
+#define MIN -3		  /* lower bound on integers considered for points */
+#define MAXNVRTS 14  	  /* maximum number of vertices */
+#define POLYDIM 3	  /* dimension of polytopes */
+#define BINLEN 3	  /* maximum length of binary number */
+#define POPSIZE 350       /* population size */
 #define NUMGEN 500        /* number of generations */
 #define NUMCUTS 1         /* number of cuts in a crossing */
 #define RANKING 0         /* macro for ranking method to select breeding pairs */
@@ -37,35 +32,37 @@
 #define MONITORON 1       /* terminal monitor on */
 #define MONITOROFF 0      /* terminal monitor off */
 
-/* fitness weights */
-#define DIST_WEIGHT 1 
-#define IP_WEIGHT 1 
-#define NVERTS_WEIGHT 0
-#define NVERTS 6
-#define NPTS_WEIGHT 0 /* if >0 slows down computation */
+/** fitness weights **/
+#define DIST_WEIGHT 1     /* distance of the facets from the origin */
+#define IP_WEIGHT 1       /* IP property */
+#define NVERTS_WEIGHT 0   /* number of vertices */
+#define NPTS_WEIGHT 0     /* number of points */
+#define H11_WEIGHT 0      /* h11 hodge number */
+#define H12_WEIGHT 0      /* h12 hodge number */
+#define H13_WEIGHT 0      /* h13 hodge number */
+#define H22_WEIGHT 0      /* h22 hodge number */
+#define EULER_WEIGHT 0    /* euler number */
+#define NVERTS 6    
 #define NPTS 7
-#define H11_WEIGHT 0 /* if >0 slows down computation */
 #define H11 1
-#define H12_WEIGHT 0 /* if >0 slows down computation */
 #define H12 1
-#define H13_WEIGHT 0 /* if >0 slows down computation */
 #define H13 1
-#define H22_WEIGHT 0 /* if >0 slows down computation */
 #define H22 1
-#define EULER_WEIGHT 0 /* if >0 slows down computation */
 #define EULER 1
 
-
+/* PALP limits */
 #define POINT_Nmax 2000000         /* maximum number of points */
 #define VERT_Nmax 128	           /* maximum number of vertices */
 #define FACE_Nmax 10000	           /* maximum number of faces */
 #define SYM_Nmax 46080             /* maximum number of symmetries */
 #define EQUA_Nmax VERT_Nmax        /* maximum number of facets */		
 #define AMBI_Dmax (5 * POLYDIM)    /* maximum dimension of the ambient space */
+#define FIB_Nmax 3000              /* maximum number of allowed weight relations among points that define the IP simplices */
 
-#define FIB_Nmax 3000 /* maximum number of allowed weight relations among points that define the IP simplices */
-
-#define GL_Long	Long /* uses W_to_GLZ like in Rat.c */
+/* data types */
+#define	Long long long    
+#define LLong long long
+#define GL_Long	Long 
 
 #define INT_Nbits 32
 #define LONG_LONG_Nbits 64
@@ -133,43 +130,43 @@ If we need more bits than can be represented by a single unsigned long long,
 these routines are designed to simulate the above definitions.
 */
 
-
-
 /*  ============           	S T R U C T U R E S              ============  */
 
-struct binary
-{
-  int list[BINLEN];  /* the actual binary list */
-};
+typedef struct {int list[BINLEN];} binary;
+/*
+A binary number as a list.
+*/
 
-struct pointlist
-{
-  int len;                        /* the number of points */
-  int points[MAXNVRTS][POLYDIM];  /* the actual point list */
-};
+typedef struct {int len; int points[MAXNVRTS][POLYDIM];} pointlist;
+/*
+A polytope state described by the convex hull of a list of points.
+pl.len is the number of points and pl.points is the actual list of points.
+*/
 
-struct bitlist
-{
-  int len;                            /* number of bits in bitlist */
-  int bits[MAXNVRTS*POLYDIM*BINLEN];  /* the actual bitlist */
-  float fitness;                      /* fitness of bitlist */
-  int terminal;                       /* terminal or not */
-};
+typedef struct {int len, terminal; float fitness; int bits[MAXNVRTS*POLYDIM*BINLEN];} bitlist;
+/*
+A bitlist state describing a polytope.
+bl.len is the length of the bitlist.
+bl.bits is the actual bitlist of 0s and 1s.
+bl.fitness is the fitness of the bitlist.
+bl.terminal is 1 if the bitlist is a terminal state and 0 otherwise.
 
-struct population
-{
-  int size;                    /* size of population */
-  float maxfitness;            /* maximal fitness in population */
-  float avfitness;             /* average fitness in population */
-  int nterm;                   /* number of terminal states in population */
-  struct bitlist bl[POPSIZE];  /* the actual population */
-};  
+*/
 
-struct NormalForm
-{
-  int nv;                       /* the number of vertices */        
-  Long x[POLYDIM][VERT_Nmax];  /* the actual normal form vertex matrix */
-}; 
+typedef struct {int size, nterm; float maxfitness, avfitness; bitlist bl[POPSIZE];} population;
+/*
+A population of bitlists of size pop.size.
+pop.maxfitness and pop.avfitness are the maximum and average fitness of the population respectively.
+pop.nterm is the number of terminal states in the population.
+pop.bl is the actual list of bitlists.
+*/  
+
+typedef struct {int nv; Long x[POLYDIM][VERT_Nmax];} NormalForm;
+/* 
+The normal form of the vertex matrix.
+NF.nv is the number of vertices.
+NF.x is the normal form matrix.
+*/
 
 typedef struct {int n, np; Long x[POINT_Nmax][POLYDIM];} PolyPointList;
 /*
@@ -233,134 +230,145 @@ The vertex permutation list.
 */
 
 
-
 /*  ============              B I T L I S T                ============  */
 
 
+bitlist pts2bts(pointlist pl);
+/* 
+Convert a pointlist into a bitlist.
+*/
 
-/* convert a decimal number into a binary list */
-struct binary decimal2binary(int num);
+pointlist bts2pts(bitlist bl);
+/* 
+Convert a bitlist into a pointlist.
+*/
 
-/* convert a binary list into a decimal number */
-int binaryToDecimal(struct binary bin);
-
-/* convert a points list into a bit list after adding max */
-struct bitlist pts2bts(struct pointlist pl);
-
-/* convert a bit list into a points list and subtract max */
-struct pointlist bts2pts(struct bitlist bl);
-
-/* generate a random bitlist of lenght len */
-struct bitlist randombitlist(int len);
-
-/* generate a random integer in a range from min to max */
 int randomint(int min, int max);
+/*
+Generate a random integer in the range from min to max.
+*/
 
-/* generate a random choice for integers 0,...,len-1 for a probability distribution p */
 int randomchoice(float p[POPSIZE], int len);
+/*
+Generate a random choice for integers 0,...,len-1 for a probability distribution p.
+*/
 
-/* write a bitlist bl to a file with file pointer fp */
-void fprintbitlist(FILE *fp, struct bitlist bl);
+void fprintbitlist(FILE *fp, bitlist bl);
+/*
+Write bitlist bl to a file with file pointer fp.
+*/
 
-/* flips bit in position pos for a bitlist bl */
-void flipbit(struct bitlist *bl, int pos);
+bitlist * freadbitlist(char *filename, int len);
+/*
+Read pointlists from a file and convert to bitlists.
+*/
 
-/* copies the bits in bitlist bl from position pos1 to pos2-1 into a new bitlist */
-struct bitlist copybitlist(struct bitlist bl, int pos1, int pos2);
+void flipbit(bitlist *bl, int pos);
+/*
+Flip bit in position pos for a bitlist bl.
+*/
 
-/* pastes bitlist blpaste into bitlist bl at position pos, overwriting previous content in bl */
-void pastebitlist(struct bitlist *bl, struct bitlist blpaste, int pos);
+void crossbitlists(bitlist *bl1, bitlist *bl2, int numcuts, int cuts[NUMCUTS]);
+/*
+Cross bitlists bl1 and bl2, with numcuts number of cuts at positions specified in array cuts.
+*/
 
-/* a simple insertion sort of an integer array into ascending order */
-void isort(int arr[], int len);
+bitlist randomstate();
+/*
+Generate a random bitlist state.
+*/
 
-/* crosses bitlists bl1 and bl2, with numcuts number of cuts at positions specified in array cuts */
-void crossbitlists(struct bitlist *bl1, struct bitlist *bl2, int numcuts, int cuts[NUMCUTS]);
-
-/* the randomstate function which generates a random state */
-struct bitlist randomstate();
-
-/* compare two bistlist by their fitness */
 int compbitlist(const void *p1, const void *p2);
+/*
+Compare two bistlist by their fitness. 
+*/
 
-/* compute the normal form of a polytope from its bitlist */
-struct NormalForm normalform(struct bitlist bl);
+NormalForm normalform(bitlist bl);
+/*
+Compute the vertex matrix normal form of a polytope from its bitlist. 
+*/
 
-/* decide if two normal forms are equal */
-int NFsequal(struct NormalForm NF1, struct NormalForm NF2);
+int NFsequal(NormalForm NF1, NormalForm NF2);
+/*
+Determine if two normal forms are equal.
+*/
 
-/* decide if two bistlist are identical */
-int bitlistsequal(struct bitlist bl1, struct bitlist bl2);
+int bitlistsequal(bitlist bl1, bitlist bl2);
+/*
+Determine if two bitlists are equal.
+*/
 
-/* decide if two bistlist describe equivalent polytopes */
-int bitlistsequiv(struct bitlist bl1, struct bitlist bl2);
-  
-  
+int bitlistsequiv(bitlist bl1, bitlist bl2);
+/*
+Determine if two bitlists are equivalent.
+*/
+
   
 /*  ============              P O P U L A T I O N                ============  */
 
 
+population randompop(int popsize);
+/*
+Generate a random population of size popsize.
+*/
 
-/* generate a random population of size popsize */
-struct population randompop(int popsize);
 
-/* sort a population by fitness */
-void sortpop(struct population *pop);
-
-/* mutate a population */
-void mutatepop(struct population *pop, float mutrate);
-
-/* find fitest in a population */
-struct bitlist fitestinpop(struct population *pop);
-
-/* find average fitness of a population */
-float avfitness(struct population *pop);
-
-/* find number of terminal state in population */
-int nterminalpop(struct population *pop);
-
-/* generate the next population from a given one */
-void nextpop(struct population pop, struct population *newpop, int meth, int numcuts, int keepfitest,
-	     float mutrate, float alpha);
-
+void nextpop(population pop, population *newpop, int meth, int numcuts, int keepfitest, 
+			float mutrate, float alpha);
+/* 
+Generate the next population from a given one by performing selection, crossover and mutation. 
+*/
 
 
 /*  ============              E V O L U T I O N                ============  */
 
 
-
-/* genetically evolve a population */
-struct population * evolvepop(struct population initialpop, int numgen, int meth, int numcuts,
+population * evolvepop(population initialpop, int numgen, int meth, int numcuts,
 			      int keepfitest, float mutrate, float alpha, int monitor);
+/*
+Evolve a given population over numgen generations.
+*/
 
-/* monitor evolution of a population */
-void monitorevol(int gen, struct population *pop);
+void monitorevol(int gen, population *pop);
+/*
+Monitor the evolution of a population.
+*/
 
-/* select terminal states from a population */
-struct bitlist * termstates(struct population *evol, int numgen, int *numterm);
+bitlist * termstates(population *evol, int numgen, int *numterm);
+/*
+Extracts the terminal states from a population.
+*/
 
-/* remove redundqncy in list of bitlists */
-void removeredundancy(struct bitlist *bl, int *len);
+void removeredundancy(bitlist *bl, int *len);
+/*
+Removes the redundancy in a list of bitlists bl of length len.
+The number of reduced states is assigned to len and the bl is sorted such that the 
+first len bitlists comprise the reduced list.
+*/
 
-/* select terminal states from a population and remove redundancy */
-struct bitlist * termstatesred(struct population *evol, int numgen, int *numterm);
+bitlist * termstatesred(population *evol, int numgen, int *numterm);
+/*
+Extracts the terminal states from a population and removes the redundancy.
+*/   
 			   
-/* repeated evolution of a random initial population, extracting terminal states */
-struct bitlist * searchenv(int numrun, int numevol, int numgen, int popsize, int meth, int numcuts,
+bitlist * searchenv(int numrun, int numevol, int numgen, int popsize, int meth, int numcuts,
 			   int keepfitest, float mutrate, float alpha, int monitor, int *numterm);
+/*
+Repeatedly evolves random initial populations extracts terminal states and saves them to a file.
+Evolves numrun*numevol populations of size popsize over numgen generations.
+*/
 
 
 /*  ============             F I T N E S S               ============  */
 
 
-
-/* the fitness function which returns the fitness of a bitlist */
-void fitness(struct bitlist * bl);
-
+void fitness(bitlist * bl);
+/*
+Computes the fitness of the bitlist bl and assigns the result to bl.fitness.
+*/
 
 
 /*  ============             V E R T E X                ============  */
-
 
 
 void Sort_VL(VertexNumList *V);
@@ -418,8 +426,7 @@ INCI Eq_To_INCI(Equation *E, PolyPointList *P, VertexNumList *VNL);
 Converts *E to an INCI.
 */
 
-void Make_Incidence(PolyPointList *P, VertexNumList *VNL, EqList *EL,
-                    FaceInfo *FI);
+void Make_Incidence(PolyPointList *P, VertexNumList *VNL, EqList *EL, FaceInfo *FI);
 /*
 Creates the structure FaceInfo *FI from *P, *VNL and *EL.
 */
@@ -429,8 +436,8 @@ int QuickAnalysis(PolyPointList *_P, BaHo *_BH, FaceInfo *_FI);
 Fast computation of FaceInfo and Hodge numbers.
 */
 
-/*  ============             N O R M A L   F O R M                ============  */
 
+/*  ============             N O R M A L   F O R M                ============  */
 
 
 void swap(int *i,int *j);
@@ -438,13 +445,7 @@ void swap(int *i,int *j);
 Swaps *i and *j.
 */
 
-int  Make_Poly_Sym_NF(PolyPointList *P, VertexNumList *VNL, EqList *EL,
-		      int *SymNum, int V_perm[][VERT_Nmax], Long NF[POLYDIM][VERT_Nmax]);
+void  Make_Poly_Sym_NF(PolyPointList *_P, VertexNumList *_V, EqList *_F, Long NF[POLYDIM][VERT_Nmax]);
 /*
-Given *P, *VNL and *EL, the following objects are determined:
-the number *SymNum of GL(n,Z)-symmetries of the polytope,
-the *SymNum vertex permutations V_perm realising these symmetries,
-the normal form coordinates NF of the vertices,
-the number of symmetries of the vertex pairing matrix
-(this number is the return value of Make_Poly_Sym_NF).
+Given *P, *VNL and *EL the normal form coordinates NF of the vertice is computed.
 */
