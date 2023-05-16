bd888d5f597cecb1920f597cee45aba95a76057a
Elli Heyes <44784866+elliheyes@users.noreply.github.com>
2023-05-16 14:11:46 +0100

    Update gen_poly.c

--- C/gen_poly.c
+++ C/gen_poly.c
@@ -11,7 +11,7 @@ int main (int narg, char* fn[]){
   int n;
   
   /* repeated evolution of a random initial population, extracting terminal states */
-  struct bitlist * bl = searchenv(1000, 1, NUMGEN, POPSIZE, ROULETTE, NUMCUTS, KEEPFITEST, MUTRATE, ALPHA, MONITORON, &n); 
+  bitlist * bl = searchenv(1000, 1, NUMGEN, POPSIZE, ROULETTE, NUMCUTS, KEEPFITEST, MUTRATE, ALPHA, MONITORON, &n); 
 
   return 0;
 } 
