1f1981e34f5a42b7d24862b449f3bf6e197b873c
Elli Heyes <44784866+elliheyes@users.noreply.github.com>
2023-05-18 14:00:50 +0100

    Update gen_poly.c

--- C/gen_poly.c
+++ C/gen_poly.c
@@ -10,6 +10,9 @@ int main (int narg, char* fn[]){
 
   int n;
   
+  /* set random number generator seed */
+  srand(clock()); 
+  
   /* repeated evolution of a random initial population, extracting terminal states */
   bitlist * bl = searchenv(1000, 1, NUMGEN, POPSIZE, ROULETTE, NUMCUTS, KEEPFITEST, MUTRATE, ALPHA, MONITORON, &n); 
 
